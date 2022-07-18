94fd923aae620a547926c6eb8ca7d3fae9132728
Aarav Jalan <aarav.jalan.aj@gmail.com>
2022-07-18 12:15:14 +0530

    Constants Renamed

--- SwerveDrive/src/main/java/frc/robot/Constants.java
+++ SwerveDrive/src/main/java/frc/robot/Constants.java
@@ -22,46 +22,46 @@ public final class Constants {
 
         public static final class FrontLeft {
             // This is the ID of the drive motor
-            public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 0;
+            public static final int DriveMotorFL = 0;
             // This is the ID of the steer motor
-            public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 0;
+            public static final int SteerMotorFL = 0;
             // This is the ID of the steer encoder
-            public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 0;
+            public static final int SteerEncoderFL = 0;
             // This is how much the steer encoder is offset from true zero (In our case,
             // zero is facing straight forward)
-            public static final double FRONT_LEFT_MODULE_STEER_OFFSET = 0;
+            public static final double SteerOffsetFL = 0;
 
         }
 
         public static final class BackRight {
-            public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 0;
-            public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 0;
-            public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 0;
-            public static final double BACK_RIGHT_MODULE_STEER_OFFSET = 0;
+            public static final int DriveMotorBR = 0;
+            public static final int SteerMotorBR = 0;
+            public static final int SteerEncoderBR = 0;
+            public static final double SteerOffsetBR = 0;
 
         }
 
         public static final class BackLeft {
-            public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 0;
-            public static final int BACK_LEFT_MODULE_STEER_MOTOR = 0;
-            public static final int BACK_LEFT_MODULE_STEER_ENCODER = 0;
-            public static final double BACK_LEFT_MODULE_STEER_OFFSET = 0;
+            public static final int DriveMotorBL = 0;
+            public static final int SteerMotorBL = 0;
+            public static final int SteerEncoderBL = 0;
+            public static final double SteerOffsetBL = 0;
 
         }
 
         public static final class FrontRight {
-            public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 0;
-            public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 0;
-            public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 0;
-            public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = 0;
+            public static final int DriveMotorFR = 0;
+            public static final int SteerMotorFR = 0;
+            public static final int SteerEncoderFR = 0;
+            public static final double SteerOffsetFR = 0;
 
         }
 
-        public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0;
-        public static final double DRIVETRAIN_WHEELBASE_METERS = 0;
-        public static final double MAX_VELOCITY_METERS_PER_SECOND = 0;
-        public static final double MAX_VOLTAGE = 0;
-        public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = 0;
+        public static final double trackwidth = 0;
+        public static final double wheelbase = 0;
+        public static final double maxVelocity = 0;
+        public static final double maxVoltage = 0;
+        public static final double maxAngularVelocity = 0;
 
     }
 
@@ -69,9 +69,9 @@ public final class Constants {
         public static final class JoySContants {
 
             public static final int joyS_ID = 0;
-            public static final int TRANSLATION_X_AXIS = 0;
-            public static final int TRANSLATION_Y_AXIS = 1;
-            public static final int ROTATION_AXIS = 5;
+            public static final int xTranslation= 0;
+            public static final int yTranslation = 1;
+            public static final int rotation = 5;
 
         }
     }
